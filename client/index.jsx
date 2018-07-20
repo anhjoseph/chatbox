0516ca417b388fd7d3807d7af24839517d6c192a
Joseph An <josephan1221@gmail.com>
2018-07-19 20:25:19 -0700

    [Feat] Basic Chatroom component

---
+++ client/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Chatroom from './components/Chatroom.jsx';
+
+ReactDOM.render(<Chatroom />, document.getElementById('Chatroom'));
