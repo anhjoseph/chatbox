e1d5b00dff995b335e3fbabca45898a270c2f5dc
Joseph An <josephan1221@gmail.com>
2018-07-21 14:27:01 -0700

    [Feat] Add message post functionality for Chatbox
    -Add componentDidMount that establishes socket connection
    -Add handleChange that sets state for typed messages
    -Add handleSubmit that sends message through socket connection

--- client/components/Chatbox.jsx
+++ client/components/Chatbox.jsx
@@ -3,14 +3,33 @@ import React from 'react';
 class Chatbox extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      message: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handlePost = this.handlePost.bind(this);
   };
+  
+  componentDidMount() {
+    const messageServer = new WebSocket('ws://localhost:3000');
+  }
+
+  handleChange(event) {
+    this.setState({
+      message: event.target.value
+    });
+  }
+
+  handlePost() {
+    messageServer.send(this.state.message);
+  }
 
   render() {
     return (
       <div>
         <form>
-          <input />
+          <input onChange={this.handleChange}/>
+          <button onSubmit={this.handlePost}></button>
         </form>
       </div>
     )
