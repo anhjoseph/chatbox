7571a02107fd5544573617b3fc0d06ac9772b3b5
Joseph An <josephan1221@gmail.com>
2018-08-11 23:56:11 -0700

    [Feat] Add Login component

--- server/index.js
+++ server/index.js
@@ -8,9 +8,32 @@ const port = 3000;
 const server = http.createServer(app);
 const io = require('socket.io').listen(server);
 
+const passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
+
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
 
 server.listen(port, () => {
   console.log(`server running at ${port}`);
@@ -23,4 +46,3 @@ server.listen(port, () => {
 
   });
 });
-
