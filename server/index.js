517828c0b0bdbb092a74f55a02b5b7e81f578daa
Joseph An <josephan1221@gmail.com>
2018-08-13 22:51:30 -0700

    Remove login.html

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const path = require('path');
 const parser = require('body-parser');
 const passport = require('passport');
+const LocalStrategy = require('passport-local');
 // const Login = require('../client/components/Login.jsx');
 // const session = require('express-session');
 
@@ -11,29 +12,27 @@ const port = 3000;
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) {
+        return done(err); 
+      }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
 app.use(passport.initialize());
-
-// passport.use(new LocalStrategy(
-//   function(username, password, done) {
-//     User.findOne({ username: username }, function (err, user) {
-//       if (err) { return done(err); }
-//       if (!user) {
-//         return done(null, false, { message: 'Incorrect username.' });
-//       }
-//       if (!user.validPassword(password)) {
-//         return done(null, false, { message: 'Incorrect password.' });
-//       }
-//       return done(null, user);
-//     });
-//   }
-// ));
-
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'login.html'));
-});
+app.use(express.static(path.join(__dirname, '../public')))
 
 server.listen(port, () => {
   console.log(`server running at ${port}`);
