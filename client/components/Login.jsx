ec618b7683b85cd218173bdeb8ecb16fe318feb9
Joseph An <josephan1221@gmail.com>
2018-08-15 02:56:30 -0700

    [Feat] Conditionally render Signup page
    -On a state toggle on Login component, render Signup component
    -Remove Authorize component

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,25 +1,50 @@
 import React from 'react';
+import Signup from './Signup.jsx';
 
-const Login = (props) => (
-  <div>
-    <form onSubmit={props.handleLogin}>
-      <div>
-        <label>Username:</label>
-        <input type="text" name="username"/>
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" name="password" onChange={props.handleChange}/>
-      </div>
-      <div>
-        <input type="submit" value="Log In" onChange={props.handleChange}/>
-      </div>
-    </form>
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasProfile: true
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    <button>
-      Sign Up
-    </button>
-  </div>
-)
+  handleClick() {
+    this.setState({
+      hasProfile: false
+    });
+  }
+
+  render() {
+    if (this.state.hasProfile) {
+      return (
+        <div>
+          <form onSubmit={this.props.handleLogin}>
+            <div>
+              <label>Username:</label>
+              <input type="text" name="username"/>
+            </div>
+            <div>
+              <label>Password:</label>
+              <input type="password" name="password" onChange={this.props.handleChange}/>
+            </div>
+            <div>
+              <input type="submit" value="Log In" onChange={this.props.handleChange}/>
+            </div>
+          </form>
+
+          <button onclick={this.handleClick}>
+            Sign Up
+          </button>
+        </div>
+      )
+    } else {
+      return (
+        <Signup />
+      )
+    }
+  }
+}
 
 export default Login;
