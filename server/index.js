d76f0750fd77397f0166e12344fa697aa7895c25
Joseph An <josephan1221@gmail.com>
2018-09-04 20:07:22 -0700

    Add util function that generates JWT
    -Remove unnecessary dependencies
    -Remove sessions logic

--- server/index.js
+++ server/index.js
@@ -2,12 +2,10 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const parser = require('body-parser');
-const session = require('express-session');
 
 const { router } = require('./router');
 const { MessageController } = require('./controllers/messages');
 const { ChannelController } = require('./controllers/channels');
-const { sessionSecret } = require('./config/session-secret.json');
 
 const app = express();
 const port = 3000;
@@ -19,19 +17,10 @@ require('../db/config');
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
-
-app.use(session({
-  secret: sessionSecret,
-  resave: false,
-  saveUninitialized: false,
-  //cookie: { secure: true }
-}));
-
 app.use('/', router);
 
 server.listen(port, () => {
   console.log(`server running at ${port}`);
-  // listen on the (native) connection event for incoming sockets
   io.on('connection', (socket) => {
 
     socket.on('user', (username) => {
---
+++ server/utils/generateToken.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+const generateToken = (user) => {
+  let payload = {
+    username: user.username
+  };
+
+  return token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: 60 * 60 * 24
+  });
+};
+
+module.exports = {
+  generateToken: generateToken
+};
