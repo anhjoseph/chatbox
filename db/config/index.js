ffa13a974026e7b773dbf259192f329ffd93bc85
Joseph An <anjoseph@Josephs-MacBook-Pro.local>
2019-07-04 21:57:57 -0700

    minor fix to db config

--- db/config/index.js
+++ db/config/index.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 const db = new Sequelize('chatroom', process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB,
+  host: process.env.DB_HOST,
   dialect: 'postgres',
 });
 
